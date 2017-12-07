021e631af02f848e80b3112dd2eee8f0c85afab8
Γιώργος ρούβας <rouvasgr@gmail.com>
2017-12-07 20:32:55 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,7 +2,7 @@ var Menu = {
 	preload: function() {
 
 		game.load.image('menu', 'assets/blue.png');
-		game.load.image('button','assets/button.png')'
+		game.load.image('button','assets/button.png');
 		//game.load.audio('tm', 'audio/bgm.mp3');
 		},
 		
