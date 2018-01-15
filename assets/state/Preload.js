57f841cffb9ceb4be26667d6b8eb1d4a2af4605f
Γιώργος ρούβας <rouvasgr@gmail.com>
2018-01-15 17:49:48 +0200

    Update Preload.js

--- assets/state/Preload.js
+++ assets/state/Preload.js
@@ -1,4 +1,15 @@
+var Preload = function (game) {}
+
+
 preload: function () {
+	 var loadingBar = this.add.sprite(this.world.centerX, this.world.centerY, 'loading');
+           loadingBar.anchor.setTo(0.5, 0.5);
+	   this.load.setPreloadSprite(loadingBar);
+	 this.game.load.image('title', 'assets/states/buttons/title.png');
+	
+	    this.game.load.image('play', 'assets/states/buttons/play.png');
+	    this.game.load.image('level2', 'assets/states/buttons/level.png');
+	
             this.load.audio('themesound','audio/new.mp3');
             this.load.audio('eatsound','audio/waka.mp3');
             
@@ -29,3 +40,9 @@ preload: function () {
 		
 		
         },
+		
+		create: function(){
+        console.log('%cPreload -> create intitialized', 'color:white; background:red');
+		this.game.state.start('MainMenu');
+	}
+}
