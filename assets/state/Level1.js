dbfb4154409ad2e7b143d78ee288c7fd628437bb
Γιώργος ρούβας <rouvasgr@gmail.com>
2018-01-18 14:05:52 +0200

    Update Level1.js

--- assets/state/Level1.js
+++ assets/state/Level1.js
@@ -249,6 +249,46 @@ Pacman.prototype = {
         }
     },
 	
+	kill_pacman: function (pacman) {
+            pacman.kill();
+            
+            lives--;
+            
+            if(lives > 1)
+                {
+                    lives = lives - 1;
+                    score = 0;
+                    game.state.star(game.state.current);
+                    alert("Caution, you have lost 1 live!");
+                    pacman.kill();
+			
+                     this.pacman.position.x = (14 * 16) + 8;
+                    this.pacman.position.y = (17 * 16) + 8;
+                    this.move(Phaser.LEFT);
+                    this.soldier.position.x = (14 * 16) + 8;
+                    this.soldier.position.y = (23 * 16) + 8;
+                     }
+                else
+                {        
+                    lives = 3;
+                    score = 0;
+			
+                    
+                    alert("You lost the game!");
+                  
+                    location.reload();
+                    time = 0;
+                    this.dots.callAll('revive');
+                     this.pacman.position.x = (14 * 16) + 8;
+                    this.pacman.position.y = (17 * 16) + 8;
+                    this.move(Phaser.LEFT);
+                    this.soldier.position.x = (14 * 16) + 8;
+                    this.soldier.position.y = (23 * 16) + 8;
+                }
+		
+            
+        },
+	
 	 enemySoldier: function () {
         if (knife_eaten == false) {
             if (this.pacman.overlap(this.knife)) {
