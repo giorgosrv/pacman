8779ad9494ca1bc29be7d221703557ee6c3e08d0
Γιώργος ρούβας <rouvasgr@gmail.com>
2018-01-18 19:59:32 +0200

    Update level2.js

--- assets/state/level2.js
+++ assets/state/level2.js
@@ -109,7 +109,7 @@ Pacman_level2 .prototype = {
         this.soldier3 = this.add.sprite((12 * 16) + 8, (29 * 16) + 8, 'soldier', 0);
         this.soldier3.anchor.set(0.5);
         this.physics.arcade.enable(this.soldier3);
-        this.soldier3.body.setSize(0, 0, 1, 100);
+        this.soldier3.body.setSize(16, 16, 0, 0);
         this.soldier3.body.velocity.x = -(this.speed);
         this.soldier3.body.velocity.y = 0;
 
