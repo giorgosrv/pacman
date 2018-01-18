2f0e7596c2a281426edcf5e53c808c30c4d2e911
Γιώργος ρούβας <rouvasgr@gmail.com>
2018-01-18 14:21:11 +0200

    Update Level1.js

--- assets/state/Level1.js
+++ assets/state/Level1.js
@@ -42,6 +42,7 @@ var Pacman = function (game) {
     this.opposites = [Phaser.NONE, Phaser.RIGHT, Phaser.LEFT, Phaser.DOWN, Phaser.UP];
     this.current = Phaser.NONE;
     this.turning = Phaser.NONE;
+	this.soldier = null;
 };
 
 Pacman.prototype = {
